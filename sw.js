82da3011d4ece0ba7805b62da6c53be7c210dcbe
Law Wai Chun <chunlaw@rocketmail.com>
2024-03-24 17:47:21 +0800

    Updates

--- sw.js
+++ sw.js
@@ -81,14 +81,14 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "url": "404.html",
     "revision": "35e096fa51bb84e124a03e7cf0038187"
   }, {
-    "url": "assets/index-DEZbCPr7.css",
+    "url": "assets/index-Cy0yfBDu.js",
     "revision": null
   }, {
-    "url": "assets/index-DYz-LTru.js",
+    "url": "assets/index-DcbBh3Rj.css",
     "revision": null
   }, {
     "url": "index.html",
-    "revision": "b9ab64687d0e1c71e60d294c82bfca62"
+    "revision": "280572760e9718903dd7f6a6b777cedc"
   }, {
     "url": "registerSW.js",
     "revision": "1872c500de691dce40960bb85481de07"
